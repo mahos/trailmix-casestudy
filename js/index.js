56ff362130864e0b763ea2e0773e707f61cbfda2
Maho Sasaki <msasaki.a@gmail.com>
2020-11-12 01:37:50 -0600

    add card flip interaction, adjust title roll animation

--- js/index.js
+++ js/index.js
@@ -5,3 +5,14 @@ $(document).ready(function() {
     });
 })
 
+$('.card').click(function() {
+    $(this).find('.rotation-plane').toggleClass('flip')
+
+
+    if ($(this).find($('.rotation-plane')).hasClass('flip')) {
+        $(this).find($('.frontside')).addClass('hideMe').css({transition: 'visibility 0.2s ease-in'});
+    } else {
+        $(this).find($('.frontside')).removeClass('hideMe');
+    }
+})
+
