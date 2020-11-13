ca33bf352c56afc2b231243398419f38dd34b72e
Maho Sasaki <msasaki.a@gmail.com>
2020-11-13 04:08:25 -0600

    add modal view for smaller images

--- js/index.js
+++ js/index.js
@@ -19,3 +19,20 @@ $('.card').click(function() {
     }
 })
 
+$('#csModal').on('show.bs.modal', function (event) {
+    if ($(window).width() < 576) {
+        console.log('window size to small!')
+        event.preventDefault();
+    }
+    console.log('modal stopped')
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var dataInfo = button.data('src');
+    console.log(dataInfo)
+    var modal = $(this)
+    if (dataInfo.length > 1) {
+        modal.find('.modal-body').html(`<iframe width=${dataInfo[1]} height=${dataInfo[2]} src=${dataInfo[0]} frameborder="0" allowfullscreen></iframe>`);
+    } else {
+        modal.find('.modal-body').html(`<img src=${dataInfo[0]} width="100%">`)
+    }
+  })
+
